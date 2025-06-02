00acfd397b44c55cc771a7c1d9016e4a20af6bf7
Marcos Vinicius <marcosvinicus444@gmail.com>
2025-06-02 17:33:00 -0300

    correção main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routes import pagamentos, usuarios, lanches, pedidos, itens_pedido, cardapio
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="API UniLanches")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite qualquer origem. Para produção, use ["https://seuapp.com"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, PUT, DELETE...)
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 app.include_router(usuarios.router, prefix="/usuarios", tags=["Usuarios"])
 app.include_router(lanches.router, prefix="/lanches", tags=["Lanches"])
@@ -9,3 +19,5 @@ app.include_router(pedidos.router, prefix="/pedidos", tags=["Pedidos"])
 app.include_router(itens_pedido.router, prefix="/itens_pedido", tags=["ItensPedido"])
 app.include_router(cardapio.router, prefix="/cardapio", tags=["CardapioDoDia"])
 app.include_router(pagamentos.router, prefix="/pagamentos", tags=["Pagamentos"])
+
+
